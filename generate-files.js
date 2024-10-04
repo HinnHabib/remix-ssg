a867054084c242d5f828ffee0cc1b24b30df3c4c
Habib Hinn <habibissahinn@gmail.com>
2024-10-04 11:00:51 +0300

    Add SSG command

--- generate-files.js
+++ generate-files.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import wget from "wget-improved";
+import waitPort from "wait-port";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
@@ -31,6 +32,10 @@ function getRouteFromFileName(fileName) {
 
 const directoryPath = path.join(__dirname, "app", "routes");
 let files = [];
+await waitPort({
+  port: 3000,
+  host: "localhost",
+});
 try {
   files = fs.readdirSync(directoryPath);
   files.forEach((file) => {
